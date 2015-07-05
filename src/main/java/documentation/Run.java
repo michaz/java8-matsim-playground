54f2012597906fddf3d007e8a9d1e67f26aafb05
Michael Zilske <michael.zilske@tu-berlin.de>
2015-07-05 14:36:04 +0200

    start dodumentation project

---
+++ src/main/java/documentation/Run.java
@@ -0,0 +1,14 @@
+package documentation;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+
+public class Run {
+
+	public static void main(String[] args) {
+		Config config = ConfigUtils.createConfig();
+
+
+	}
+
+}
