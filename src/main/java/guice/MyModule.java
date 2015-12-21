7e6f4576bc781001abf574f64f0f4e7e29e67d5f
Michael Zilske <michael.zilske@tu-berlin.de>
2015-12-21 16:47:10 +0100

    Have to initialize counts container in the module I'm graphing.

--- src/main/java/guice/MyModule.java
+++ src/main/java/guice/MyModule.java
@@ -1,10 +1,13 @@
 package guice;
 
 import org.matsim.analysis.IterationStopWatch;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.controler.*;
 import org.matsim.core.controler.corelisteners.ControlerDefaultCoreListenersModule;
 import org.matsim.core.scenario.ScenarioByInstanceModule;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.counts.Counts;
 
 
 class MyModule extends AbstractModule {
@@ -12,7 +15,9 @@ class MyModule extends AbstractModule {
     public void install() {
         install(new ControlerDefaultsModule());
         install(new ControlerDefaultCoreListenersModule());
-        install(new ScenarioByInstanceModule(ScenarioUtils.loadScenario(getConfig())));
+        Scenario scenario = ScenarioUtils.createScenario(getConfig());
+        scenario.addScenarioElement(Counts.ELEMENT_NAME, new Counts<Link>());
+        install(new ScenarioByInstanceModule(scenario));
         bind(OutputDirectoryHierarchy.class).asEagerSingleton();
         bind(IterationStopWatch.class).asEagerSingleton();
         bind(ControlerI.class).to(Controler.class).asEagerSingleton();
